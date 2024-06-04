c6f5f14888769ae57fbecd50331729ade55656c2
pranjal30 <pranjalp677@gmail.com>
2024-06-04 11:02:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("button");
+document.body.addEventListener("click", function(){
+    const randomColor ="#" + Math.floor(Math.random() * 16777216).toString(16);
+    // console.log(randomColor);
+    document.body.style.backgroundColor = randomColor;
+});
+
+
+
+
